1f3684b8b51d177245260da641fbb6a7a838481a
Lucas <lucasluizss@live.com>
2020-06-18 19:19:50 -0300

    Adicionando base de testes (em andamento)

---
+++ backend/src/__tests__/account.spec.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import app from '../';
+
+describe('Likes', () => {
+  // it('should be able to give a like to the repository', async () => {
+  //   const repository = await request(app)
+  //     .post('/repositories')
+  //     .send({
+  //       url: 'https://github.com/Rocketseat/umbriel',
+  //       title: 'Umbriel',
+  //       techs: ['Node', 'Express', 'TypeScript']
+  //     });
+
+  //   let response = await request(app).post(
+  //     `/repositories/${repository.body.id}/like`
+  //   );
+
+  //   expect(response.body).toMatchObject({
+  //     likes: 1
+  //   });
+
+  //   response = await request(app).post(
+  //     `/repositories/${repository.body.id}/like`
+  //   );
+
+  //   expect(response.body).toMatchObject({
+  //     likes: 2
+  //   });
+  // });
+
+  it('should not be able to like a repository that does not exist', async () => {
+    await request(app)
+      .post(`/users/123`)
+      .expect(400);
+  });
+});
